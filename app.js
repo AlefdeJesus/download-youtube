3bcfde15a24b4cc45227f80ea7b10350082da055
AlefdeJesus <alefjesus1996st.s@gmail.com>
2024-06-28 11:22:25 -0300

    adicionado o caminho relativo do video para o front end baixar

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const {downloadVideo} = require('./downloadVideo.js')
 const path = require('path')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json())
 
-app.post('/download',async (req,res)=>{
-    const {url} = req.body;
-    console.log(url)
-    if(!url){
-        return res.status(400).json({error: 'URL do video é necessária'});
-    }
-    try {
-        const filePath =  await downloadVideo(url);
-        res.status(200).json({message: 'Download concluido com sucesso', filePath})
-    } catch (error) {
-        res.status(500).json({error:'Erro ao baixar o video'})
-    }
+app.use(cors( {
+  origin: '*',
+  methods: 'GET, POST, OPTIONS, PATCH, DELETE, PUT',
+  allowedHeaders: 'Content-Type, Authorization, Accept, X-Requested-With, credentials', // Certifique-se de incluir 'credentials'
+  credentials: true
+}));
+
+app.post('/download', async (req, res) => {
+  const { url } = req.body;
+  console.log(url);
+  if (!url) {
+      return res.status(400).json({ error: 'URL do vídeo é necessária' });
+  }
+  try {
+      const filePath = await downloadVideo(url);
+      console.log('filePath:', filePath);  // Adicione este log para depuração
+
+      // Retorna o caminho relativo
+      const relativeFilePath = path.relative(__dirname, filePath).replace(/\\/g, '/');
+      res.status(200).json({ message: 'Download concluído com sucesso', filePath: relativeFilePath });
+  } catch (error) {
+      console.error('Erro ao baixar o vídeo:', error);
+      res.status(500).json({ error: 'Erro ao baixar o vídeo' });
+  }
 });
 
+
 app.get('/videos/:filename', (req, res) => {
-    const { filename } = req.params;
-    const filePath = path.join(__dirname, 'videos', filename);
-    
-    res.download(filePath, (err) => {
-      if (err) {
-        console.error('Erro ao fornecer o vídeo:', err);
-        res.status(500).send('Erro ao fornecer o vídeo.');
-      }
-    });
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, 'videos', filename);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error('Erro ao fornecer o vídeo:', err);
+      res.status(500).send('Erro ao fornecer o vídeo.');
+    }
   });
+}); 
 //const videoUrl = 'https://youtu.be/DDJX_vor5N8?si=bo4zJJAf1eIdyCT7';
 //downloadVideo(videoUrl);
 
--- downloadVideo.js
+++ downloadVideo.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const path = require('path')
-const url = require('./app.js');
-const { error } = require('console');
+const path = require('path');
 
 // Função para baixar o vídeo na maior resolução com o nome do YouTube
 async function downloadVideo(url) {
-  if(!url || typeof url !== 'string'){
-    throw new Error('URL inválida!')
+  if (!url || typeof url !== 'string') {
+    throw new Error('URL inválida!');
   }
   try {
     // Obtendo informações do vídeo
@@ -17,46 +15,46 @@ async function downloadVideo(url) {
     const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo', filter: 'videoandaudio' });
     console.log('Formato encontrado:', format);
 
-    // Nome do arquivo baseado no título do vídeo, e removendo caracteres da url do video, como de short
+    // Nome do arquivo baseado no título do vídeo, e removendo caracteres inválidos e emojis
     const title = info.videoDetails.title
-    .replace(/[<>:"\/\\|?*]+/g, '')
-    .replace(/[\u{1F600}-\u{1F64F}]/gu, '')  // Emoticons
-    .replace(/[\u{1F300}-\u{1F5FF}]/gu, '')  // Símbolos e pictogramas
-    .replace(/[\u{1F680}-\u{1F6FF}]/gu, '')  // Transporte e símbolos de mapa
-    .replace(/[\u{1F700}-\u{1F77F}]/gu, '')  // Símbolos diversos e pictogramas
-    .replace(/[\u{1F780}-\u{1F7FF}]/gu, '')  // Símbolos suplementares de pictogramas
-    .replace(/[\u{1F800}-\u{1F8FF}]/gu, '')  // Símbolos suplementares de flechas
-    .replace(/[\u{1F900}-\u{1F9FF}]/gu, '')  // Símbolos suplementares de emoticons
-    .replace(/[\u{1FA00}-\u{1FA6F}]/gu, '')  // Símbolos de actividades
-    .replace(/[\u{1FA70}-\u{1FAFF}]/gu, '')  // Símbolos de objetos
-    .replace(/[\u{2600}-\u{26FF}]/gu, '')    // Símbolos diversos
-    .replace(/[\u{2700}-\u{27BF}]/gu, '');   // Dingbats;
-    //const output = `${title}.mp4`;
+      .replace(/[<>:"\/\\|?*]+/g, '')
+      .replace(/[\u{1F600}-\u{1F64F}]/gu, '')  // Emoticons
+      .replace(/[\u{1F300}-\u{1F5FF}]/gu, '')  // Símbolos e pictogramas
+      .replace(/[\u{1F680}-\u{1F6FF}]/gu, '')  // Transporte e símbolos de mapa
+      .replace(/[\u{1F700}-\u{1F77F}]/gu, '')  // Símbolos diversos e pictogramas
+      .replace(/[\u{1F780}-\u{1F7FF}]/gu, '')  // Símbolos suplementares de pictogramas
+      .replace(/[\u{1F800}-\u{1F8FF}]/gu, '')  // Símbolos suplementares de flechas
+      .replace(/[\u{1F900}-\u{1F9FF}]/gu, '')  // Símbolos suplementares de emoticons
+      .replace(/[\u{1FA00}-\u{1FA6F}]/gu, '')  // Símbolos de atividades
+      .replace(/[\u{1FA70}-\u{1FAFF}]/gu, '')  // Símbolos de objetos
+      .replace(/[\u{2600}-\u{26FF}]/gu, '')    // Símbolos diversos
+      .replace(/[\u{2700}-\u{27BF}]/gu, '');   // Dingbats
+
     const output = path.join(__dirname, 'videos', `${title}.mp4`);
 
-    // Baixando o vídeo e salvando no arquivo de saída
-    ytdl(url, { format: format })
-      .pipe(fs.createWriteStream(output))
-      .on('finish', () => {
-        console.log(`Download concluído: ${output}`);
-      })
-      .on('error', (err) => {
-        console.error(`Erro durante o download: ${err.message}`);
-        fs.unlinkSync(output)
-        //criar uma funçao para excluir video quando der erro durante download
-      });
+    // Certifique-se de que o diretório 'videos' existe
+    if (!fs.existsSync(path.join(__dirname, 'videos'))) {
+      fs.mkdirSync(path.join(__dirname, 'videos'));
+    }
 
-    // Exibindo informações do vídeo no console
-    console.log('Título:', info.videoDetails.title);
-    console.log('Duração:', info.videoDetails.lengthSeconds, 'segundos');
-  
+    return new Promise((resolve, reject) => {
+      ytdl(url, { format: format })
+        .pipe(fs.createWriteStream(output))
+        .on('finish', () => {
+          console.log(`Download concluído: ${output}`);
+          resolve(output);  // Retorna o caminho completo do arquivo
+        })
+        .on('error', (err) => {
+          console.error(`Erro durante o download: ${err.message}`);
+          fs.unlinkSync(output);
+          reject(err);
+        });
+    });
+
   } catch (err) {
     console.error(`Erro ao obter informações do vídeo: ${err.message}`);
-    throw err
+    throw err;
   }
 }
 
-// URL do vídeo que você quer baixar
-
-
-module.exports = {downloadVideo};
+module.exports = { downloadVideo };
